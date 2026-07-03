40a4df6c8017b67540a9ec66a4ddfc74a07cdac6
and1ru <andres.vanegas2700@gmail.com>
2026-07-02 23:51:02 -0500

    se agregaron comentarios describiendo que hace cada cosa en el test

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,11 @@
 import {describe, expect, it} from 'vitest'
 import {sum} from './index.ts'
 
+// decribe -> organiza los test por grupos
 describe("sum funcion", () => {
+    // it -> es un caso de prueba, puede ser positivo o negativo
     it("deberia retornar 3",() => {
+        // expect -> dice que esperamos como resultado
         expect(sum(1,2)).toBe(3)
     })
 })
