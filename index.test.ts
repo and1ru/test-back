c846be627e80ddc3914fc3d257d5b390eab9d7cc
and1ru <andres.vanegas2700@gmail.com>
2026-07-03 00:35:27 -0500

    se explicaron nuevas cosas

--- index.test.ts
+++ index.test.ts
@@ -1,11 +1,48 @@
-import {describe, expect, it} from 'vitest'
-import {sum} from './index.ts'
+import {describe, beforeAll, beforeEach, afterEach, afterAll, it, expect} from 'vitest'
 
-// decribe -> organiza los test por grupos
-describe("sum funcion", () => {
-    // it -> es un caso de prueba, puede ser positivo o negativo
-    it("deberia retornar 3",() => {
-        // expect -> dice que esperamos como resultado
-        expect(sum(1,2)).toBe(3)
+describe("auth", () => {
+    // pueden haber describers anidados
+    describe("login",() => {
+        beforeAll(() => {
+            console.log("se ejecuta antes del test, solo una vez")
+        })
+
+        beforeEach(() => {
+            console.log("se ejecuta antes de cada test")
+        })
+
+        afterEach(() => {
+            console.log("se ejecuta despues de cada test")
+        })
+
+        afterAll(() => {
+            console.log("se ejecuta despues del test, solo una vez")
+        })
+
+        it("test 1", () => {
+            console.log("test 1")
+        })
+
+        it("test 2", () => {
+            console.log("test 2")
+        })
+    })
+
+    describe("register",() => {
+        expect(1+1).toBe(2)
+
+        // usar toEqual en vez de toBe porque toBe compara la referencias y en objetos nunca seran iguales en cambio toEqual compara solo el valor
+        expect({nombre:"andres"}).toEqual({nombre:"andres"})
+
+        // toBeTruthy -> valida que el valor sea truthy
+        expect(true).toBeTruthy()
+
+        // toBeFalsy -> valida que el valor sea falsy
+        expect(false).toBeFalsy()
+
+        // toThrow -> valida que una funcion lance un error
+        expect(() => {
+            throw new Error("error")
+        }).toThrow()
     })
 })
